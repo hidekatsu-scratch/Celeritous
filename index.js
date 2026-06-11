66f05b6e9c1339962eed03e9c21c29465e0d2207
hidekatsu <3scr@proton.me>
2026-06-11 19:39:45 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+(() => {
+  const getLanguageFromCookie = () => {
+    const cookieValue = document.cookie.match(/(^|;) ?language=([^;]*)(;|$)/);
+    return cookieValue ? cookieValue[2] : "ja";
+  };
+  const lang = getLanguageFromCookie();
+  const contentMap = {
+    ja: {
+      title: "Celeritous - プロジェクトをより高速に",
+      html: `<div class="info"><p>Scratchのプロジェクトを高速稼働</p><p>ゲーム、アニメ、計算をなめらかに</p><div class="try"><img src="/img/try.svg"><botton>試してみる</booton></div>
+             <div class="LatestInfo"><h2>最新の情報</h2><a>更に表示</a><div class="LatestInfo_con"></div>
+             <div class="update"><h2>最新のアップデート</h2><a>更に表示</a><div class="update_con"></div>`
+    },
+    en: {
+      title: "Celeritous - The Faster Scratch Mod",
+      html: `<div class="info"><p>Run Scratch project faster</p><p>Smooth gaming, animation, and processing</p><div class="try"><img src="/img/try.svg"><botton>Try it now!</booton></div>
+             <div class="LatestInfo"><h2>Latest News</h2><a>View more</a><div class="LatestInfo_con"></div>
+             <div class="update"><h2>Recent Update</h2><a>View more</a><div class="update_con"></div>`
+    }
+  }
+  const currentContent = contentMap[lang] || contentMap["ja"];
+  document.title = currentContent.title;
+  document.head.insertAdjacentHTML("beforeend", `
+    <meta charset="utf-8">
+    <link rel="icon" href="/favicon.ico">
+  `);
+  document.title = currentContent.title;
+  const mainDiv = document.querySelector(".main");
+  if (mainDiv) {
+    mainDiv.innerHTML = currentContent.html;
+  }
+  const scriptTag = document.createElement("script");
+  scriptTag.src = "standard.js";
+  scriptTag.defer = true; 
+  document.head.appendChild(scriptTag);
+})();
