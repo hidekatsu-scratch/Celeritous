e7c8f987b44600c98631e706cde15f9bae929ee9
hidekatsu <3scr@proton.me>
2026-07-02 18:12:00 +0900

    Update analy.js

--- js/analy.js
+++ js/analy.js
@@ -5,6 +5,7 @@
   gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-L7VLTDKNMM';
   gaScript.async = true;
   head.insertBefore(gaScript, firstChild);
+
   const inlineScript = document.createElement('script');
   inlineScript.textContent = `
     window.dataLayer = window.dataLayer || [];
@@ -13,4 +14,13 @@
     gtag('config', 'G-L7VLTDKNMM');
   `;
   head.insertBefore(inlineScript, firstChild);
+  const clarityScript = document.createElement('script');
+  clarityScript.textContent = `
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xg1nujf6h6");
+  `;
+  head.insertBefore(clarityScript, firstChild);
 })();
