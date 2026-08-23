4d68439785e1cf9aeea691a943e0b5d33eab4afa
hidekatsu <3scr@proton.me>
2026-08-23 10:19:02 +0900

    Delete js/maincss.js

--- js/maincss.js
+++
@@ -1,8 +0,0 @@
-
-(() => {
-  document.head.insertAdjacentHTML("beforeend", `
-    <link rel="stylesheet" href="/css/header.css">
-    <link rel="stylesheet" href="/css/footer.css">
-    <link rel="stylesheet" href="/css/mainhtml.css">
-  `);
-})();
